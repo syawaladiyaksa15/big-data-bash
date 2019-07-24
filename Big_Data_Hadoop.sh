d17818f8415ee0dc35eb89152e9eeae744ab58a6
syawaladiyaksa15 <50990522+syawaladiyaksa15@users.noreply.github.com>
2019-07-24 16:32:34 +0800

    menambahkan file big_data.sh

---
+++ Big_Data_Hadoop.sh
@@ -0,0 +1,11 @@
+export DataNodeDNS= "ec2-3-84-61-191.compute-1.amazonaws.com"
+export DataNode001DNS= "ec2-18-207-208-47.compute-1.amazonaws.com"
+export DataNode002DNS= "ec2-18-233-170-132.compute-1.amazonaws.com"
+export DataNode003DNS= "ec2-18-212-64-119.compute-1.amazonaws.com"
+
+export DataNodeDNS="3.84.61.191"
+export DataNode001DNS="18.207.208.47"
+export DataNode002DNS="18.233.170.132"
+export DataNode003DNS="18.212.64.119"
+
+export IdentityFile= "../export/hadoopkey.pem"
